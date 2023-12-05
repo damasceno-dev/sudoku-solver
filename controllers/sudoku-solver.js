e62f3fd2e65cf02b6d0a418c350d6549f6967320
Replit user <>
2023-12-05 21:52:32 +0000

    first commit: finished getRows and getColumns methods

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,10 +1,47 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+
+    if (puzzleString.length !== 81) {
+      return "Expected puzzle to be 81 characters long";
+    }
+    
+    puzzleArray = puzzleString.split('');
+    puzzleArray.forEach(element => {
+      if (!/\d/.test(element) && !/./.test(element)) {
+        return "Invalid characters in puzzle";
+      }
+    })
+
+    return true;
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  getRows(puzzleString) {
+      let row = []
+      let rows = []
+    puzzleString.split('').forEach((e, i) => {
+      row.push(e)
+      if (i > 7 && ((i + 1) % 9 === 0)) {
+          rows.push(row);
+        row = [];
+      }
+    })
+    return rows;
+  }
+  
+  getColumns(puzzleString) {
+    const cols = Array.from({ length: 9 }, () => []);
+    const rows = this.getRows(puzzleString);
+    rows.forEach((arr, x) => {
+      arr.forEach((e,i) => {
+        cols[i].push(e)
+      })
+    })
+    return cols;
+  }
 
+  checkRowPlacement(puzzleString, row, column, value) {    
+    const rows = getRows(puzzleString)
   }
 
   checkColPlacement(puzzleString, row, column, value) {
--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,16 @@
 
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
+const sudoku = new SudokuSolver();
 module.exports = function (app) {
   
   let solver = new SudokuSolver();
 
   app.route('/api/check')
     .post((req, res) => {
+      console.log(req.body)
+
+      sudoku.getColumns(req.body.puzzle)
 
     });
     
